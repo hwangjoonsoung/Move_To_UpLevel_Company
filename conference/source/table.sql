661ec7d8db48c130fbcba857de833f633b568e00
hwangjoonsoung <joonsoung93@naver.com>
2025-09-18 21:48:51 +0900

    feat: creat table (fee, fee_item, registrations_fee_item, registration, data_edit_log)

---
+++ conference/source/table.sql
@@ -0,0 +1,66 @@
+create table Registration
+(
+    id              int auto_increment
+        primary key,
+    good_name       varchar(50) null,
+    price           int         null,
+    amount          int         null,
+    pay_method      varchar(10) null,
+    trade_number    int         null,
+    order_number    int         null,
+    date_of_payment datetime    null,
+    pay_status      tinyint(1)  null,
+    name            varchar(30) null,
+    affiliation     varchar(50) null,
+    position        varchar(50) null,
+    email           varchar(50) null,
+    phone           varchar(30) null,
+    registered      tinyint(1)  null,
+    date_of_regist  datetime    null,
+    date_of_create  datetime    null,
+    member_id       int         null
+);
+
+create table data_edit_log
+(
+    id              int auto_increment
+        primary key,
+    edit_table_id   int          null,
+    edit_table_name varchar(10)  null,
+    date_of_update  datetime     null,
+    edit_by         varchar(100) null,
+    edit_reason     varchar(255) null
+);
+
+create table fee
+(
+    id   int auto_increment
+        primary key,
+    name varchar(50) null,
+    memo text        null
+);
+
+create table fee_item
+(
+    id     int auto_increment
+        primary key,
+    fee_id int         not null,
+    Field  int         null,
+    Field2 varchar(10) null,
+    Field3 tinyint(1)  null,
+    constraint FK_fee_TO_fee_item_1
+        foreign key (fee_id) references fee (id)
+);
+
+create table registrations_fee_item
+(
+    fee_item_id     int not null,
+    registration_id int not null,
+    primary key (fee_item_id, registration_id),
+    constraint FK_Registration_TO_registrations_fee_item_1
+        foreign key (registration_id) references Registration (id),
+    constraint FK_fee_item_TO_registrations_fee_item_1
+        foreign key (fee_item_id) references fee_item (id)
+);
+
+
