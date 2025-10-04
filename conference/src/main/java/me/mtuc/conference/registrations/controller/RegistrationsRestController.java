c18a8835df264d2afd1beba651e2300e107b75f3
hwangjoonsoung <joonsoung93@naver.com>
2025-10-04 11:00:55 +0900

    feat: valid 추가

--- conference/src/main/java/me/mtuc/conference/registrations/controller/RegistrationsRestController.java
+++ conference/src/main/java/me/mtuc/conference/registrations/controller/RegistrationsRestController.java
@@ -1,11 +1,13 @@
 package me.mtuc.conference.registrations.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.mtuc.conference.registrations.dto.RegistrationRequestDto;
 import me.mtuc.conference.registrations.dto.RegistrationsIdResponseDto;
 import me.mtuc.conference.registrations.service.RegistrationsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -18,20 +20,20 @@ public class RegistrationsRestController {
 
     private final RegistrationsService registrationsService;
 
-    @PostMapping("/registrations/new")
-    public ResponseEntity<RegistrationsIdResponseDto> newRegistrations(@RequestBody RegistrationRequestDto registrationRequestDto) throws URISyntaxException {
+    @PostMapping(value = "/registrations/new", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<RegistrationsIdResponseDto> newRegistrations(@Valid @RequestBody RegistrationRequestDto registrationRequestDto) throws URISyntaxException {
         Long id = registrationsService.newRegistrations(registrationRequestDto);
-        URI uri = new URI("/registrations/");
+        URI uri = new URI("/registrations/"+id+"/payment");
         return ResponseEntity.created(uri).body(new RegistrationsIdResponseDto(id));
     }
 
-    @PostMapping("/registrations/{id}/edit")
-    public ResponseEntity<RegistrationsIdResponseDto> editRegistrations(@PathVariable(name = "id") Long id, @RequestBody RegistrationRequestDto registrationRequestDto) {
+    @PostMapping(value = "/registrations/{id}/edit", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<RegistrationsIdResponseDto> editRegistrations(@PathVariable(name = "id") Long id, @Valid @RequestBody RegistrationRequestDto registrationRequestDto) {
         Long editRegistrationsId = registrationsService.editRegistrations(id, registrationRequestDto);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(new RegistrationsIdResponseDto(editRegistrationsId));
     }
 
-    @PostMapping("/registrations/{id}/remove")
+    @PostMapping(value = "/registrations/{id}/remove",consumes = "application/json", produces = "application/json")
     public ResponseEntity<RegistrationsIdResponseDto> removeRegistrations(@PathVariable(name = "id") Long id) {
         Long removedRegistrationId = registrationsService.removeRegistration(id);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(new RegistrationsIdResponseDto(removedRegistrationId));
--- conference/src/main/java/me/mtuc/conference/registrations/dto/RegistrationRequestDto.java
+++ conference/src/main/java/me/mtuc/conference/registrations/dto/RegistrationRequestDto.java
@@ -1,5 +1,6 @@
 package me.mtuc.conference.registrations.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,14 +12,28 @@ import java.time.LocalDate;
 @Builder
 public class RegistrationRequestDto {
 
+    @NotBlank(message = "상품명을 입력해 주세요")
     private String goodName;
+
+    @NotBlank(message = "가격 코드를 입력해 주세요")
     private Long feeItemId;
+    @NotBlank(message = "금액을 선택해 주세요")
     private int price;
+    @NotBlank(message = "성명을 입력해 주세요")
+    @Size(max = 100, message = "성명은 최대 100자 까지 입력가능하니다.")
     private String name;
+    @NotBlank(message = "생년월일을 입력해 주시기 바랍니다.")
+    @Size(min = 8,max = 8, message = "생년월일을 8자로 입력해 주시기 바랍니다. ex)19990909")
+    @Pattern(regexp = "^[0-9]+$",message = "-을 제외하고 입력해 주시기 바랍니다.")
     private LocalDate birth;
+    @NotBlank(message = "소속을 입력해 주시기 바랍니다")
     private String affiliation;
+    @NotBlank(message = "직책을 입력해 주시기 바랍니다.")
     private String position;
+    @NotBlank(message = "이메일을 입력해 주시기 바랍니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
+    @NotBlank(message = "전화번호를 입력해 주시기 바랍니다.")
     private String phone;
 
 }
