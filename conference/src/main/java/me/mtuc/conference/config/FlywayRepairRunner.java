fdb1eb0565bce12f64e58bd3e92af47345b8c88b
hwangjoonsoung <joonsoung93@naver.com>
2025-11-03 19:56:35 +0900

    chore: flyway Reapir 주석처리

---
+++ conference/src/main/java/me/mtuc/conference/config/FlywayRepairRunner.java
@@ -0,0 +1,16 @@
+package me.mtuc.conference.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayRepairRunner {
+
+//    @Bean
+//    public FlywayMigrationStrategy flywayMigrationStrategy() {
+//        return flyway -> {
+//            flyway.repair();
+//            flyway.migrate();
+//        };
+//    }
+
+}
