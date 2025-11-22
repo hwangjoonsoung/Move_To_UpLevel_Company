f1bae9fbf0cf7d46d9ff2786ae814b3e8f37d0b4
hwangjoonsoung <joonsoung93@naver.com>
2025-11-22 12:32:21 +0900

    feat: security 적용중

--- conference/src/main/java/me/mtuc/conference/config/SecurityConfig.java
+++ conference/src/main/java/me/mtuc/conference/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package me.mtuc.conference.config;
 
+import jakarta.servlet.FilterChain;
 import lombok.RequiredArgsConstructor;
 import me.mtuc.conference.filter.JwtFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
@@ -12,5 +16,10 @@ public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/user/login",""))
+    }
 }
 
